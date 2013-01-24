46bb0c1dcf133aac83ebd3e1b890254cc107442a
Skaronator <DayZ@Skaronator.com>
2013-01-24 14:02:41 +0100

    Upload Lingor Code (Part 5) / Fix #1 #6

---
+++ SQF/lingor_roads/config.cpp
@@ -0,0 +1,15 @@
+#define _ARMA_
+
+
+//Class config.bin{
+class CfgPatches
+{
+ class lingor_roads
+ {
+  units[] = {};
+  weapons[] = {};
+  requiredVersion = 0.1;
+  requiredAddons[] = {};
+ };
+};
+//};
