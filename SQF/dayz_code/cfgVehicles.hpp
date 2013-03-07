69c7a605ae339c22db42054e2ea98886012c9037
Skaronator <DayZ@Skaronator.com>
2013-03-07 14:49:51 +0100

    Re aad MV22 (Mistake)

--- SQF/dayz_code/cfgVehicles.hpp
+++ SQF/dayz_code/cfgVehicles.hpp
@@ -785,6 +785,29 @@ class CfgVehicles {
 		transportMaxMagazines = 80;
 		transportmaxbackpacks = 15;
 	};
+	//MV22
+	class MV22;
+	class MV22_DZ: MV22
+	{
+		displayname = "MV-22 Speed Plane";
+		displaynameshort = "MV-22";
+		scope = 2;
+		side = 2;
+		crew = "";
+		typicalCargo[] = {};
+		hiddenSelections[] = {};
+		class TransportMagazines{};
+		class TransportWeapons{};
+		weapons[] = {};
+		magazines[] = {};
+		gunnerHasFlares = false;
+		commanderCanSee = 2+16+32;
+		gunnerCanSee = 2+16+32;
+		driverCanSee = 2+16+32;
+		transportMaxWeapons = 20;
+		transportMaxMagazines = 100;
+		transportmaxbackpacks = 15;
+	};
 	class House {
 		class DestructionEffects;
 	};
