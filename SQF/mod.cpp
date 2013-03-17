bc2a282092d641cec4aec0118b9c3a187c6ed860
Skaronator <DayZ@Skaronator.com>
2013-03-17 10:53:09 +0100

    Update

--- SQF/mod.cpp
+++ SQF/mod.cpp
@@ -1,4 +1,4 @@
 dir = "@dayzlingorskaro";
-name = "DayZ Lingor (Skaronator.com)";
-actionName = "Website";
+name = "Arma 2: DayZ Lingor (Skaronator.com)";
+actionName = $STR_MOD_LAUNCHER_ACTION;
 action = "http://www.skaronator.com";
