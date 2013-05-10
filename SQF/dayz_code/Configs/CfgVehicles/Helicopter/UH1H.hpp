3a484a3e3b1ced9c0b171b038d24e6d992caa579
Skaronator <DayZ@Skaronator.com>
2013-05-10 23:09:49 +0200

    Add New Huey Skin + Sound

--- SQF/dayz_code/Configs/CfgVehicles/Helicopter/UH1H.hpp
+++ SQF/dayz_code/Configs/CfgVehicles/Helicopter/UH1H.hpp
@@ -20,7 +20,6 @@
 		side = 2;
 		crew = "";
 		typicalCargo[] = {};
-		hiddenSelections[] = {};
 		class TransportMagazines{};
 		class TransportWeapons{};
 		commanderCanSee = 2+16+32;
@@ -29,7 +28,45 @@
 		transportMaxWeapons = 5;
 		transportMaxMagazines = 25;
         transportmaxbackpacks = 4;
-		
+		class Sounds {
+			class Engine {
+				sound[] = {"\lingor_skaronator\vehicle\uh1\snd\uh1_t.wss", 2.51189, 1, 1500};
+				frequency = "rotorSpeed";
+				volume = "camPos*((rotorSpeed-0.72)*4)";
+			};
+			
+			class RotorLowOut {
+				sound[] = {"\lingor_skaronator\vehicle\uh1\snd\uh1_rb.wss", 2.51189, 1, 2000};
+				frequency = "rotorSpeed";
+				volume = "camPos*(0 max (rotorSpeed-0.1))";
+				cone[] = {1.6, 3.14, 2, 0.5};
+			};
+			
+			class RotorHighOut {
+				sound[] = {"\lingor_skaronator\vehicle\uh1\snd\uh1_ra.wss", 2.51189, 1, 2000};
+				frequency = "rotorSpeed";
+				volume = "camPos*10*(0 max (rotorThrust-0.9))";
+				cone[] = {1.6, 3.14, 2, 0.5};
+			};
+			
+			class EngineIn {
+				sound[] = {"\lingor_skaronator\vehicle\uh1\snd\uh1_c.wss", 3.16228, 1};
+				frequency = "rotorSpeed";
+				volume = "(1-camPos)*((rotorSpeed-0.75)*4)";
+			};
+			
+			class RotorLowIn {
+				sound[] = {"\lingor_skaronator\vehicle\uh1\snd\uh1_rb.wss", 3.16228, 1};
+				frequency = "rotorSpeed";
+				volume = "2*(1-camPos)*((rotorSpeed factor[0.3, 1.1]) min (rotorSpeed factor[1.1, 0.3]))";
+			};
+			
+			class RotorHighIn {
+				sound[] = {"\lingor_skaronator\vehicle\uh1\snd\uh1_ra.wss", 3.16228, 1};
+				frequency = "rotorSpeed";
+				volume = "(1-camPos)*3*(rotorThrust-0.9)";
+			};
+		};
 		class Turrets : Turrets 
 		{
 			class MainTurret : MainTurret 
@@ -41,4 +78,5 @@
 				magazines[] = {"100Rnd_762x51_M240"};
 			};
 		};
+		hiddenSelectionsTextures[] = {"lingor_skaronator\vehicle\uh1\uh1_co.paa", "lingor_skaronator\vehicle\uh1\uh1_in_co.paa", "lingor_skaronator\vehicle\uh1\default_co.paa"};
 	};
